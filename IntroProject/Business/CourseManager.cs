830ce532fc133593395cfe837469d60739a48130
ahguney <ahmetguney090@gmail.com>
2024-02-11 22:58:13 +0300

    Add project files.

---
+++ IntroProject/Business/CourseManager.cs
@@ -0,0 +1,30 @@
+﻿using IntroProject.DataAccess.Abstracts;
+using IntroProject.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroProject.Business
+{
+    public class CourseManager
+    {
+        //Dependency Injection
+        private readonly ICourseDal _courseDal;
+
+        public CourseManager(ICourseDal courseDal)
+        {
+            _courseDal = courseDal;
+        }
+
+        public List<Course> GetAll()
+        {
+
+
+            return _courseDal.GetAll();
+        }
+
+
+    }
+}
---
+++ IntroProject/DataAccess/Abstracts/ICourseDal.cs
@@ -0,0 +1,15 @@
+﻿using IntroProject.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroProject.DataAccess.Abstracts
+{
+    public interface ICourseDal
+    {
+        List<Course> GetAll();
+        void Add(Course course);
+    }
+}
